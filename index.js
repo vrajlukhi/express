e4ec3159ba1e66af690b7bbf005fe0b3b9ee69a7
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2023-09-19 11:15:19 +0530

    method

--- index.js
+++ index.js
@@ -2,33 +2,41 @@ const express=require("express")
 const exp=express()
 exp.use(express.json())
 
-exp.get("/",(req,res)=>{
- res.status(200).send("Home Page")
+const db = [
+    {
+        name: "vraj",
+        grid: 2533,
+        course: "backend",
+    }
+]
+exp.get("/student",(req,res)=>{
+    res.status(200).send(db)
 })
-exp.get("/login",(req,res)=>{
- res.status(200).send("login Page")
+exp.post("/student",(req,res)=>{
+    let newstudent={
+        name:req.body.name,
+        grid:req.body.grid,
+        course:req.body.course
+    }
+    db.push(newstudent)
+    res.status(201).send(newstudent)
 })
-exp.get("/signup",(req,res)=>{
- res.status(200).send("signup Page")
+exp.patch("/student/:id",(req,res)=>{
+    res.status(200).send("student enter")
+    let {id}=req.params
+    
+    let updatedStudent=db.filter(stud=>stud.id==id)
+    updatedStudent[0].name = req.body.name
+    updatedStudent[0].grid = req.body.grid
+    updatedStudent[0].course = req.body.course
+    res.status(200).send(...updatedStudent)
 })
-
-exp.post("/",(req,res)=>{
-    console.log(req.body);
-    res.status(200).send(req.body)
+app.delete("/student/:id", (req, res) => {
+    let {id }= req.params 
+    let deletestudents = db.filter(student => student.id != id)
+    console.log(deletestudents)
+    res.send(...deletestudents)
 })
-exp.post("/signup",(req,res)=>{
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-exp.post("/login",(req,res)=>{
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-exp.post("/content",(req,res)=>{
-    console.log(req.body);
-    res.status(200).send(req.body)
-})
-
 exp.listen(7777,()=>{
     console.log("Server 7777 start");
 })
